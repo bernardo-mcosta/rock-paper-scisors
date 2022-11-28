03dd06c18e8eada6b7cce7cba5f0f4b6d1aaf49c
Bernardo <76459310+bernardo-mcosta@users.noreply.github.com>
2022-11-28 17:19:25 -0300

    Add basic function to play rounds

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice(){
-    const choices = ['Rock','Paper','Scissors']
+    const choices = ['ROCK','PAPER','SCISSORS']
     return choices[Math.floor(Math.random() * choices.length)]
 }
 //console.log(getComputerChoice())
@@ -9,10 +9,32 @@ function getPlayerChoice(){
     while (playerChoice.toLocaleUpperCase() != 'ROCK' && playerChoice.toLocaleUpperCase() != 'PAPER' && playerChoice.toLocaleUpperCase() != 'SCISSORS'){
         playerChoice = prompt('Your choice is not valid. Choose between Rock, Paper or Scissors.')
     }
-    return playerChoice
+    return playerChoice.toLocaleUpperCase()
 }
 
+//let playerChoice = getPlayerChoice()
+//let computerChoice = getComputerChoice()
 
+let playerChoice = 'ROCK'
+let computerChoice = 'SCISSORS'
 
-getPlayerChoice()
-//|| playerChoice.toLocaleUpperCase != 'PAPER' || playerChoice.toLocaleUpperCase != 'SCISSORS' 
+function playGame(playerChoice,computerChoice){
+    console.log (`You chose ${playerChoice} and the computer chose ${computerChoice}.` )
+    if (playerChoice == 'ROCK' && computerChoice == 'SCISSORS'){
+        console.log('You win!')
+        return 
+    }
+}
+   
+playGame(playerChoice,computerChoice)
+/* 
+    ROCK - PAPER
+    ROCK - SCISSORS
+    ROCK - ROCK
+    PAPER - ROCK
+    PAPER - SCISSORS
+    PAPER - PAPER
+    SCISSORS - PAPER
+    SCISSORS - ROCK
+    SCISSORS - SCISSORS
+    */
