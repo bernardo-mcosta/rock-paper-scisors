c50e2916651d138e25e0618a37621681e3a23333
Bernardo <76459310+bernardo-mcosta@users.noreply.github.com>
2022-11-28 16:58:44 -0300

    Add function to get choice from the player

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ function getComputerChoice(){
     return choices[Math.floor(Math.random() * choices.length)]
 }
 //console.log(getComputerChoice())
+
+function getPlayerChoice(){
+    let playerChoice = prompt('Choose between Rock, Paper or Scissors.')
+    while (playerChoice.toLocaleUpperCase() != 'ROCK' && playerChoice.toLocaleUpperCase() != 'PAPER' && playerChoice.toLocaleUpperCase() != 'SCISSORS'){
+        playerChoice = prompt('Your choice is not valid. Choose between Rock, Paper or Scissors.')
+    }
+    return playerChoice
+}
+
+
+
+getPlayerChoice()
+//|| playerChoice.toLocaleUpperCase != 'PAPER' || playerChoice.toLocaleUpperCase != 'SCISSORS' 
